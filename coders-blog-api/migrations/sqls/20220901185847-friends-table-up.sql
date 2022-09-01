e4be2d94f400300422109a5d5b23eefac585705b
Crypt00o <octopus2672001@gmail.com>
2022-09-01 21:04:05 +0200

    Add Friends Migration(SQL,js)

---
+++ coders-blog-api/migrations/sqls/20220901185847-friends-table-up.sql
@@ -0,0 +1,10 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+CREATE TABLE friends(
+friend1_id uuid DEFAULT uuid_generate_v4() NOT NULL,
+friend2_id uuid DEFAULT uuid_generate_v4() NOT NULL CHECK(friend2_id IS DISTINCT FROM friend1_id),
+friendship_status BOOLEAN NOT NULL,
+friendship_date DATE NOT NULL DEFAULT now() ,
+PRIMARY KEY(friend1_id,friend2_id),
+FOREIGN KEY(friend1_id) REFERENCES users(user_id),
+FOREIGN KEY(friend2_id) REFERENCES users(user_id)
+);
