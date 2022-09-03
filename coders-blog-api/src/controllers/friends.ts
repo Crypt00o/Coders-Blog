363c6f9dd06f0075bf6c9c8b8b701d7cb1349014
Crypt00o <octopus2672001@gmail.com>
2022-09-03 05:27:20 +0200

    Update Friends Controller

--- coders-blog-api/src/controllers/friends.ts
+++ coders-blog-api/src/controllers/friends.ts
@@ -105,30 +105,63 @@ const _delete=async(req:Request,res:Response):Promise<void>=>{
 
 const getFriends=async(req:Request,res:Response):Promise<void>=>{
     try{
-
+        if(validator.uuid_v4_Validator(req.params.user_id)){
+            const friends:Array<Friend>=await friendmodel.getFriends(req.params.user_id as string)
+            if(friends.length==0){
+                res.status(200).json({user_id:req.params.user_id,total_friends:friends.length,friends:friends,message:"this user haven,t friends untill now"})
+            }
+            else{
+                res.status(200).json({user_id:req.params.user_id,total_friends:friends.length,friends:friends})
+            }
+        }
+        else{
+            res.status(400).json({"Error": "This is Not Valid User ID"})
+        }
     }
     catch(err){
-
+        res.status(400).json({"Error": "User ID Not Found"})
     }
 }
 
 
 const getRecievedRequests=async(req:Request,res:Response):Promise<void>=>{
     try{
-
+        if(validator.uuid_v4_Validator(req.params.user_id)){
+            const friends:Array<Friend>=await friendmodel.getRecievedRequests(req.params.user_id as string)
+            if(friends.length==0){
+                res.status(200).json({user_id:req.params.user_id,total_recieved_request:friends.length,friendship_requests:friends,message:"you haven,t friendship_requests untill now"})
+            }
+            else{
+                res.status(200).json({user_id:req.params.user_id,total_recieved_request:friends.length,friendship_requests:friends})
+            }
+        }
+        else{
+            res.status(400).json({"Error": "This is Not Valid User ID"})
+        }
     }
     catch(err){
-
+        res.status(400).json({"Error": "User ID Not Found"})
     }
 }
 
 
 const getSendedRequests= async(req:Request,res:Response):Promise<void>=>{
     try{
-
+        if(validator.uuid_v4_Validator(req.params.user_id)){
+            const friends:Array<Friend>=await friendmodel.getSendedRequests(req.params.user_id as string)
+            if(friends.length==0){
+                res.status(200).json({user_id:req.params.user_id,total_sended_request:friends.length,friendship_requests:friends,message:"you haven,t send friendship_requests untill now"})
+            }
+            else{
+                res.status(200).json({user_id:req.params.user_id,total_sended_request:friends.length,friendship_requests:friends})
+            }
+        }
+        else{
+            res.status(400).json({"Error": "This is Not Valid User ID"})
+        }
     }
     catch(err){
-
+        res.status(400).json({"Error": "User ID Not Found"})
     }
 }
 
