0474c5797ef11844808b31ab2ed5a7e333785753
Crypt00o <octopus2672001@gmail.com>
2022-09-03 05:40:59 +0200

    Added Friends Router And Update index Router

--- coders-blog-api/src/routes/index.ts
+++ coders-blog-api/src/routes/index.ts
@@ -3,6 +3,7 @@ import { welcomeMessage, notFound } from '../controllers/main.controller'
 import {join} from 'path'
 import { usersRouter } from './api/users/user'
 import { articlesRouter } from './api/articles/articles'
+import { friendsRouter } from './api/friends/friends'
 //Declareing Static Directory for Serving Static Files 
 
 const staticDir:string=join(__dirname,'..','..','static')
@@ -22,9 +23,13 @@ router.get('/', welcomeMessage)
 // Useing User Router With /users/ Endpoint
 router.use('/users', usersRouter)
 
-// Response With Not Found for any invalid path
+// Useing Article Router With /articles/ Endpoint
 router.use("/articles",articlesRouter)
 
+// Useing Friends Router With /friends/ Endpoint
+router.use("/friends",friendsRouter)
+
+// Response With Not Found for any invalid path
 router.all('/*', notFound)
 
 export default router
