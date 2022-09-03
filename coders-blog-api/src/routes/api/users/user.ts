226cfe13390f70643f1654aca10670c02e712b4c
Crypt00o <octopus2672001@gmail.com>
2022-09-03 13:35:51 +0200

    Update User Router

--- coders-blog-api/src/routes/api/users/user.ts
+++ coders-blog-api/src/routes/api/users/user.ts
@@ -5,4 +5,5 @@ const usersRouter = Router()
 usersRouter.route("/").get(authenticateing, userController.index).post(userController.create)
 usersRouter.route("/:user_id").get(authenticateingByTokenId , userController.show).patch(authenticateingByTokenId , userController.update).delete(authenticateingByTokenId , userController._delete)
 usersRouter.route("/login").post(userController.login)
+usersRouter.get("/:user_id/info",authenticateing,userController.generalInfo)
 export {usersRouter}
